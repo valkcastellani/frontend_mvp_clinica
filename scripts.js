3279c5cd062805e6eb578524dfdd657f516b255e
Valk Castellani <valkcastellani@gmail.com>
2024-04-12 22:12:10 -0300

    Implementação da exclusão de pacientes

--- scripts.js
+++ scripts.js
@@ -1,3 +1,54 @@
+const abreFechaModal = () => {
+  let sessionPaciente = document.getElementById("novoPaciente");
+  if (sessionPaciente.classList.contains("modal-visible")) {
+    sessionPaciente.classList.remove("modal-visible"); // Remove a classe "modal-visible"
+  } else {
+    sessionPaciente.classList.add("modal-visible"); // Adiciona a classe "modal-visible"
+  }
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para deletar um paciente da tabela e do servidor via requisição DELETE
+  --------------------------------------------------------------------------------------
+*/
+const removerPaciente = (cpf, nome) => {
+  Swal.fire({
+    title: "Tem certeza?",
+    text: "Você vai excluir o paciente " + nome + " portador do CPF " + cpf + "!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "var(--cor-button-confirm)",
+    cancelButtonColor: "var(--cor-button-cancel)",
+    confirmButtonText: "Sim, pode apagar!",
+    cancelButtonText: "Cancelar"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      let url = 'http://127.0.0.1:5000/paciente?cpf=' + cpf;
+      fetch(url, {
+        method: 'delete'
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          let linhaTabela = document.getElementById(cpf);
+          linhaTabela.remove();
+          Swal.fire({
+            title: "Excluído!",
+            text: data.message,
+            icon: "success"
+          });
+        })
+        .catch((error) => {
+          Swal.fire({
+            title: 'Error!',
+            text: data.message,
+            icon: 'error'
+          })
+        });
+    }
+  });
+}
+
 /*
   --------------------------------------------------------------------------------------
   Função para obter a lista existente do servidor via requisição GET
@@ -10,7 +61,9 @@ const getList = async () => {
   })
     .then((response) => response.json())
     .then((data) => {
-      data.produtos.forEach(item => insertList(item.cpf, item.nome, item.data_nascimento, item.telefone, item.email, item.data_insercao))
+      data.pacientes.forEach(paciente => insertList(paciente.cpf, paciente.nome,
+        paciente.data_nascimento, paciente.telefone, paciente.email,
+        paciente.data_insercao))
     })
     .catch((error) => {
       console.error('Error:', error);
@@ -25,102 +78,83 @@ const getList = async () => {
 getList()
 
 
-/*
-  --------------------------------------------------------------------------------------
-  Função para colocar um item na lista do servidor via requisição POST
-  --------------------------------------------------------------------------------------
-*/
-const postItem = async (inputProduct, inputQuantity, inputPrice) => {
-  const formData = new FormData();
-  formData.append('nome', inputProduct);
-  formData.append('quantidade', inputQuantity);
-  formData.append('valor', inputPrice);
+// /*
+//   --------------------------------------------------------------------------------------
+//   Função para colocar um item na lista do servidor via requisição POST
+//   --------------------------------------------------------------------------------------
+// */
+// const postItem = async (inputProduct, inputQuantity, inputPrice) => {
+//   const formData = new FormData();
+//   formData.append('nome', inputProduct);
+//   formData.append('quantidade', inputQuantity);
+//   formData.append('valor', inputPrice);
 
-  let url = 'http://127.0.0.1:5000/produto';
-  fetch(url, {
-    method: 'post',
-    body: formData
-  })
-    .then((response) => response.json())
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-}
-
-
-/*
-  --------------------------------------------------------------------------------------
-  Função para criar um botão close para cada item da lista
-  --------------------------------------------------------------------------------------
-*/
-const insertButton = (parent) => {
-  let span = document.createElement("span");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  parent.appendChild(span);
-}
+//   let url = 'http://127.0.0.1:5000/produto';
+//   fetch(url, {
+//     method: 'post',
+//     body: formData
+//   })
+//     .then((response) => response.json())
+//     .catch((error) => {
+//       console.error('Error:', error);
+//     });
+// }
 
 
 /*
   --------------------------------------------------------------------------------------
-  Função para remover um item da lista de acordo com o click no botão close
+  Função para criar botões: alterar, excluir e visualizar
   --------------------------------------------------------------------------------------
 */
-const removeElement = () => {
-  let close = document.getElementsByClassName("close");
-  // var table = document.getElementById('tablePacientes');
-  let i;
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      let div = this.parentElement.parentElement;
-      const nomeItem = div.getElementsByTagName('td')[0].innerHTML
-      if (confirm("Você tem certeza?")) {
-        div.remove()
-        deleteItem(nomeItem)
-        alert("Removido!")
-      }
-    }
-  }
+const insertButtons = (parent, cpf, nome) => {
+  let buttonAlterar = document.createElement("button");
+  let iconAlterar = document.createElement("i");
+  iconAlterar.className = "fa-solid fa-pencil";
+  buttonAlterar.className = "button button-table";
+  buttonAlterar.appendChild(iconAlterar);
+  buttonAlterar.addEventListener("click", () => alterarPaciente(cpf));
+
+  let buttonVisualizar = document.createElement("button");
+  let iconVisualizar = document.createElement("i");
+  iconVisualizar.className = "fa-solid fa-eye"
+  buttonVisualizar.className = "button button-table";
+  buttonVisualizar.appendChild(iconVisualizar);
+  buttonVisualizar.addEventListener("click", () => consultarPaciente(cpf));
+
+  let buttonExcluir = document.createElement("button");
+  let iconExcluir = document.createElement("i");
+  iconExcluir.className = "fa-solid fa-trash-can";
+  buttonExcluir.className = "button button-table";
+  buttonExcluir.appendChild(iconExcluir);
+  buttonExcluir.addEventListener("click", () => removerPaciente(cpf, nome));
+
+  parent.appendChild(buttonAlterar);
+  parent.appendChild(buttonExcluir);
+  parent.appendChild(buttonVisualizar);
 }
 
-/*
-  --------------------------------------------------------------------------------------
-  Função para deletar um item da lista do servidor via requisição DELETE
-  --------------------------------------------------------------------------------------
-*/
-const deleteItem = (item) => {
-  console.log(item)
-  let url = 'http://127.0.0.1:5000/produto?nome=' + item;
-  fetch(url, {
-    method: 'delete'
-  })
-    .then((response) => response.json())
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-}
 
-/*
-  --------------------------------------------------------------------------------------
-  Função para adicionar um novo item com nome, quantidade e valor 
-  --------------------------------------------------------------------------------------
-*/
-const newItem = () => {
-  let inputProduct = document.getElementById("newInput").value;
-  let inputQuantity = document.getElementById("newQuantity").value;
-  let inputPrice = document.getElementById("newPrice").value;
+// /*
+//   --------------------------------------------------------------------------------------
+//   Função para adicionar um novo item com nome, quantidade e valor
+//   --------------------------------------------------------------------------------------
+// */
+// const newItem = () => {
+//   let inputProduct = document.getElementById("newInput").value;
+//   let inputQuantity = document.getElementById("newQuantity").value;
+//   let inputPrice = document.getElementById("newPrice").value;
 
-  if (inputProduct === '') {
-    alert("Escreva o nome de um item!");
-  } else if (isNaN(inputQuantity) || isNaN(inputPrice)) {
-    alert("Quantidade e valor precisam ser números!");
-  } else {
-    insertList(inputProduct, inputQuantity, inputPrice)
-    postItem(inputProduct, inputQuantity, inputPrice)
-    alert("Item adicionado!")
-  }
-}
+//   if (inputProduct === '') {
+//     alert("Escreva o nome de um item!");
+//   } else if (isNaN(inputQuantity) || isNaN(inputPrice)) {
+//     alert("Quantidade e valor precisam ser números!");
+//   } else {
+//     insertList(inputProduct, inputQuantity, inputPrice)
+//     postItem(inputProduct, inputQuantity, inputPrice)
+//     alert("Item adicionado!")
+//   }
+// }
+
 
 /*
   --------------------------------------------------------------------------------------
@@ -128,18 +162,16 @@ const newItem = () => {
   --------------------------------------------------------------------------------------
 */
 const insertList = (cpf, nome, data_nascimento, telefone, email, data_insercao) => {
-  var item = [cpf, nome, data_nascimento, telefone, email, data_insercao]
-  var table = document.getElementById('tablePacientes').getElementsByTagName('tbody');
+  const novaDataNascimento = new Date(data_nascimento).toLocaleDateString()
+  const novaDataInsercao = new Date(data_insercao).toLocaleString('pt-BR');
+  var item = [cpf, nome, novaDataNascimento, telefone, email, novaDataInsercao]
+  var table = document.getElementById('tabelaPacientes');
   var row = table.insertRow();
+  row.id = cpf
 
   for (var i = 0; i < item.length; i++) {
     var cel = row.insertCell(i);
     cel.textContent = item[i];
   }
-  insertButton(row.insertCell(-1))
-  // document.getElementById("newInput").value = "";
-  // document.getElementById("newQuantity").value = "";
-  // document.getElementById("newPrice").value = "";
-
-  removeElement()
+  insertButtons(row.insertCell(-1), cpf, nome)
 }
